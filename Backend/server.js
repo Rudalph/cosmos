f3c6ff674fe531309a2a3e6d557a2b80ba5047a5
Rudalph <gonsalvesrudalph@gmail.com>
2025-06-28 21:30:54 +0530

    Server updated with keep alive route

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const apodRouter = require("./routes/apodRouter");
 const nasaImageRouter = require('./routes/nasaImageRouter');
 const asteroidRouter = require('./routes/neowsRouter')
 const epicRouter = require('./routes/epicRouter');
+const keepAlikeRouter = require('./routes/keepAliveRouter')
+const axios = require('axios');
 
 const app = express();
 const PORT = 5000;
@@ -14,5 +16,15 @@ app.use("/api/apod", apodRouter);
 app.use("/api/nasa", nasaImageRouter);
 app.use('/api/asteroids', asteroidRouter);
 app.use('/api/epic', epicRouter);
+app.use('/api', keepAlikeRouter);
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+const SELF_URL = 'https://your-render-url.onrender.com/api/ping'; // Replace with your actual deployed URL
+
+setInterval(() => {
+  axios.get(SELF_URL)
+    .then(() => console.log('Self-ping successful'))
+    .catch(err => console.error('Self-ping failed:', err.message));
+}, 5 * 60 * 1000);
